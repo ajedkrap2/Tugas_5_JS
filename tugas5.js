f4dbff7864a3bdcbb4ce62a5ea91fbae9cd81e6d
ajedkrap <mr.thedja@gmail.com>
2020-10-07 00:19:02 +0700

    submit

---
+++ tugas5.js
@@ -0,0 +1,21 @@
+function tugas5(){
+  x = 1;
+  /*
+  while (x <= 20){
+    if (!(x % 3) && x < 16){
+        process.stdout.write(x + ", ");
+      }
+    x++;
+  }
+  process.stdout.write("dst.");
+  console.log("");
+  */
+  while (x <= 20){
+    if (!(x % 3)){
+        console.log(x);
+      }
+    x++;
+  }
+}
+
+tugas5();
